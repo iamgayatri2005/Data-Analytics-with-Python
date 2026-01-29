461e7302a298421cb9a5bf2298da3e173f921965
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-01-30 01:26:19 +0530

    update code

---
+++ sets/set_methods.py
@@ -0,0 +1,21 @@
+items = {"apple", "banana", "tomato"}
+
+# items.add("orange")
+# print(items)
+
+# items.update(["mango", "Peach"])
+# print(items)
+
+# items.remove("banana") 
+# print(items)
+
+# items.remove("bananae") # Error
+# items.discard("bananae") # No error
+# print(items)
+
+a = items.pop()
+print(items)
+print(a)
+
+items.clear()
+print(items)
