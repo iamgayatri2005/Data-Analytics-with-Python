bd1dfee046ec7c2fd4e07e80232935b6d1079fc3
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-01-30 21:52:42 +0530

    update code

---
+++ Dictionary-Python/basics.py
@@ -0,0 +1,16 @@
+student = {
+    "name": "Gayu",
+    "city": "BBSR",
+    "comapny": "Microsoft"
+}
+
+print(student["comapny"])
+# print(student["nameeee"]) #Error
+
+# print(student.get("nameeeeeee"))
+# print(student.get("name"))
+
+student["city"] = "Patia"
+print(student)
+
+
