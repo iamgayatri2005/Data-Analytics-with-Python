d2bc05d4d21efba714c29455b7039199da1f6b8f
iamgayatri2005 <hiiamgayatri15@gmail.com>
2026-02-06 21:40:55 +0530

    update code

---
+++ Local vs Global Variables/global_variable.py
@@ -0,0 +1,8 @@
+x = 78 # local variable
+def show_value():
+    x = 72
+    print(x)
+
+show_value()
+show_value()
+print(x)    
---
+++ Local vs Global Variables/local_variables.py
@@ -0,0 +1,7 @@
+def show_value():
+    x = 11 # local variable
+    print(x)
+
+show_value()
+x = 78 # global variable
+show_value()    
